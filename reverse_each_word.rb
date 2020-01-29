2fc6dcff36e088f1a0368cf2e18fb407f8d06b03
Timur Mishiev <timurmishiev@gmail.com>
2020-01-29 05:28:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,15 @@ return_array << string.reverse
 end
 return_array.join(" ")
 end
+
+def reverse_each_word(string)
+array = string.split(" ") #turn string into an array
+new_array = []
+array.collect do|string| #iterate over the array
+end
+new_array << string.reverse #reverse each word in
+the array
+new_array.join(" ")
+end
+ reverse_each_word("Hello there, and how are you?")
+ #=> "olleH ,ereht dna woh era ?uoy"
