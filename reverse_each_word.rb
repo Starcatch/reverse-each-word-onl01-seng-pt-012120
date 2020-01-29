18d07ae3f1935bd8fdbf91fd6310012096ffd769
Timur Mishiev <timurmishiev@gmail.com>
2020-01-29 05:31:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,11 +8,10 @@ return_array.join(" ")
 end
 
 def reverse_each_word(string)
-array = string.split(" ") #turn string into an array
+array = string.split(" ") 
 new_array = []
 array.collect do |string| #iterate over the array
 end
-new_array << string.reverse #reverse each word in
-the array
+new_array << string.reverse
 new_array.join(" ")
 end
